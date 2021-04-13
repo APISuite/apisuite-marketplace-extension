676ed73d313830c828b87527e8133e8691be7400
Diogo Simoes <diogo.miguel.simoes@gmail.com>
2021-04-14 00:18:09 +0100

    chore: build configs

--- build.sh
+++ build.sh
@@ -5,16 +5,16 @@ echo ${DOCKER_PASS} | docker login --username ${DOCKER_USER} --password-stdin
 HASH=$(git rev-parse --short HEAD)
 
 docker build \
-  -t cloudokihub/apisuite-billing-extension:$HASH \
-  -t cloudokihub/apisuite-billing-extension:latest .
+  -t cloudokihub/apisuite-marketplace-extension:$HASH \
+  -t cloudokihub/apisuite-marketplace-extension:latest .
 
-docker push cloudokihub/apisuite-billing-extension:$HASH
-docker push cloudokihub/apisuite-billing-extension:latest
+docker push cloudokihub/apisuite-marketplace-extension:$HASH
+docker push cloudokihub/apisuite-marketplace-extension:latest
 
 
 if [ "$CIRCLE_BRANCH" = "main" ]; then
   VERSION=$(cat package.json | grep version | head -1 | awk -F ": " '{ print $2 }' | sed 's/[",]//g')
 
-  docker build -t cloudokihub/apisuite-billing-extension:$VERSION .
-  docker push cloudokihub/apisuite-billing-extension:$VERSION
+  docker build -t cloudokihub/apisuite-marketplace-extension:$VERSION .
+  docker push cloudokihub/apisuite-marketplace-extension:$VERSION
 fi
